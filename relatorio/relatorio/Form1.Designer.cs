560b430eada0189cfe82f7e37de031c149072878
MarcioJuniorDev <marciojunior2911@gmail.com>
2025-10-23 14:53:56 -0300

    relatorio

---
+++ relatorio/relatorio/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace relatorio
+{
+    partial class frmMapa
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // frmMapa
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackgroundImage = Properties.Resources.MAPA1;
+            ClientSize = new Size(900, 600);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmMapa";
+            Text = "Form1";
+            TransparencyKey = Color.Lime;
+            Load += frmMapa_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ relatorio/relatorio/Form1.cs
@@ -0,0 +1,15 @@
+namespace relatorio
+{
+    public partial class frmMapa : Form
+    {
+        public frmMapa()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMapa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
